ba243171f3360c5f24f1516eb5b73242f2f089e6
tgavankar <tgavankar@gmail.com>
2012-03-24 16:45:41 -0400

    Sample demo + jFreeChart

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+	 public static void main(String[] args) {
+	        PieChart demo = new PieChart("Comparison", "Which operating system are you using?");
+	        demo.pack();
+	        demo.setVisible(true);
+	    }
+}
