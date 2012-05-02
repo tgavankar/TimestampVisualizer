cc941e4f1e3fa9d4aec6cf6302dfa58651371cc8
Lillian Michalek <lmichale@andrew.cmu.edu>
2012-05-02 16:53:14 -0400

    Improved filtering interface

--- src/panels/ViewPanel.java
+++ src/panels/ViewPanel.java
@@ -6,6 +6,7 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 
+import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -15,6 +16,9 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
 import visualize.DateRange;
 import actions.RedrawAction;
@@ -29,23 +33,24 @@ public class ViewPanel {
 	public DateRange range;
 	String title;
 	
+	
 	int offset = 0;
 	Dimension d;
 	Color c;
 	
 	public JRadioButton yearsButton = new JRadioButton("Year");
 	public JRadioButton monthsButton = new JRadioButton("Month");
-	public JRadioButton daysOfWeekButton = new JRadioButton("Day of week");
+	public JRadioButton daysOfWeekButton = new JRadioButton("Day");
 	public JRadioButton hoursButton = new JRadioButton("Hour");
 	public JRadioButton minutesButton = new JRadioButton("Minute");
 	public JRadioButton secondsButton = new JRadioButton("Second");
 	
-	public JList yearList;
-	public JList monthList;
-	public JList dayList;
-	public JList hourList;
-	public JList minuteList;
-	public JList secondList;
+	public JList<String> yearList;
+	public JList<String> monthList;
+	public JList<String> dayList;
+	public JList<String> hourList;
+	public JList<String> minuteList;
+	public JList<String> secondList;
 	
 	
 	public ViewPanel(MasterPanel master, DateRange r, Dimension d, Color c, String s)
@@ -61,8 +66,14 @@ public class ViewPanel {
 		panel.setPreferredSize(d);
         panel.setBackground(c);
         
+        
+      
+        
+        
         GridBagConstraints constraints = getConstraints();
 
+        
+        
         ButtonGroup g = new ButtonGroup();
         g.add(yearsButton);
         g.add(monthsButton);
@@ -72,47 +83,147 @@ public class ViewPanel {
         g.add(secondsButton);
        
         
-        addTitle();
-        addYears();
-        addMonths();
-        addDaysOfWeek();
-        addHours();
-        addMinutes();
-        addSeconds();
-        
-        addYearList();
-        addMonthsList();
-        addDaysList();
-        addHoursList();
-        addMinutesList();
-        addSecondsList();
-        
-        constraints.gridx = 2;
-        constraints.gridy = offset++;
-        constraints.gridwidth = 1;
-		constraints.anchor = GridBagConstraints.CENTER;
 
-		panel.add(new JLabel(" "), constraints);
+        
+        
+        
+        ////////////// start of button panel
+        
+        constraints.gridy = offset++;
+        constraints.gridx = 0;
+        constraints.weighty = 0;
+        constraints.insets = new Insets(0,10,0,10);
+        
+        panel.add(new JLabel(" "), constraints);
+        
+        
+        if(title.indexOf("Single")==-1)
+        	createComboBox();
+        
+        constraints.gridy = offset++;
+        panel.add(new JLabel(" "), constraints);
+        
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridBagLayout());
+        buttonPanel.setBackground(c);
+        
+        Border line = BorderFactory.createEtchedBorder(EtchedBorder.RAISED);
+        TitledBorder border = BorderFactory.createTitledBorder(line,
+        		"View data by...",
+        		TitledBorder.LEFT,
+        		TitledBorder.TOP);
+        buttonPanel.setBorder( border);
+        
+        constraints.gridy = offset++;
+        constraints.gridx = 0;
+        constraints.weighty = 1;
+		panel.add(buttonPanel, constraints);
+        
 		
+		
+        addYears(buttonPanel);
+        addMonths(buttonPanel);
+        addDaysOfWeek(buttonPanel);
+        addHours(buttonPanel);
+        addMinutes(buttonPanel);
+        addSeconds(buttonPanel);
+        
+        
+		
+		
+		
+		///////////////// BUFFER between button and list panels
+        
+        constraints.gridy = offset++;
+        constraints.gridx = 0;
+        constraints.weighty = 0;
+        panel.add(new JLabel(" "), constraints);
+        
+        
+        
+        ///////////////// start of list panel
+        
+        
+        JPanel listPanel = new JPanel();
+        listPanel.setLayout(new GridBagLayout());
+        listPanel.setBackground(c);
+        
+        Border line2 = BorderFactory.createEtchedBorder(EtchedBorder.RAISED);
+        TitledBorder border2 = BorderFactory.createTitledBorder(line2,
+        		"Select data to include...",
+        		TitledBorder.LEFT,
+        		TitledBorder.TOP);
+        listPanel.setBorder( border2);
+        
+        constraints.gridy = offset++;
+        constraints.gridx = 0;
+        constraints.weighty = 1;
+        panel.add(listPanel, constraints);
+        
+        
+        
+        addYearList(listPanel);
+        addMonthsList(listPanel);
+        addDaysList(listPanel);
+        addHoursList(listPanel);
+        addMinutesList(listPanel);
+        addSecondsList(listPanel);
+        
+        
+        constraints.gridx = 0;
+        constraints.gridy = offset++;
+		constraints.anchor = GridBagConstraints.CENTER;
+		
+		
+/////////////// BUTTON SECTION
+		
+		
+		//constraints.weighty = 0;
+		panel.add(new JLabel(" "), constraints);
 		constraints.gridy = offset++;
 		
+		constraints.insets = new Insets(0,110,0,110);
         final JButton bSubmit = new JButton("Apply");
         bSubmit.addActionListener(new RedrawAction(m));
-
-        /*final JButton export = new JButton("Export");
-        export.addActionListener(new ExportAction(m));*/
-        
         panel.add(bSubmit, constraints);
         
-        /*constraints.gridy = offset++;
-        panel.add(new JLabel(" "), constraints);
         constraints.gridy = offset++;
-        panel.add(export, constraints);*/
+        panel.add(new JLabel(" "), constraints);
+		
+        
+        // Add border to the main panel.
+        /*
+        Border line = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
+        TitledBorder border = BorderFactory.createTitledBorder(line,"Select filters");
+        panel.setBorder( border);
+        */
         
 	}
 	
 	
-	public void addSecondsList()
+	public void createComboBox()
+	{
+		GridBagConstraints c = new GridBagConstraints();
+		String[] choices = new String[2];
+		choices[0] = "Graph 1 (top)";
+		choices[1] = "Graph 2 (bottom)";
+		JComboBox<String> box = new JComboBox<String>(choices);
+			
+		if(title.indexOf("1") != -1)
+			box.setSelectedIndex(0);
+		else if(title.indexOf("2") != -1)
+			box.setSelectedIndex(1);
+		
+		box.addActionListener(new SwitchSidePanelAction(m));
+		
+		c.gridx = 0;
+		c.gridy = offset++;
+		c.insets = new Insets(0,10,0,10);
+		panel.add(box,c);
+	}
+	
+	
+	public void addSecondsList(JPanel panel)
 	{
 		String[] sec = new String[range.seconds.length+1];
 		sec[0] = "Select all";
@@ -120,7 +231,7 @@ public class ViewPanel {
 		{
 			sec[i] = ""+(i-1);
 		}
-		JList list = new JList(sec);
+		JList<String> list = new JList<String>(sec);
 		list.setVisibleRowCount(7);
 
 		int[] ind = new int[1];
@@ -131,28 +242,21 @@ public class ViewPanel {
 		
 		GridBagConstraints c = new GridBagConstraints();
 		
-		offset -=2;
-		
-		c.gridx = 3;
-		c.gridy = offset++;
+		c.gridx = 2;
+		c.gridy = 3;
 		c.gridwidth = 1;
-		c.anchor = GridBagConstraints.CENTER;
-		c.insets = new Insets(10, 0, 0, 0);
-		
+		c.insets = new Insets(10,5,0,5);
 
 		
 		panel.add(new JLabel("Second"),c);
-		
-		//c.anchor = GridBagConstraints.WEST;
-
-		c.gridy = offset++;
-		c.anchor = GridBagConstraints.WEST;
+		c.insets = new Insets(0,5,10,5);
+		c.gridy = 4;
 
 		panel.add(sp, c);
 	}
 	
 	
-	public void addMinutesList()
+	public void addMinutesList(JPanel panel)
 	{
 		String[] min = new String[range.minutes.length+1];
 		min[0] = "Select all";
@@ -160,7 +264,7 @@ public class ViewPanel {
 		{
 			min[i] = ""+(i-1);
 		}
-		JList list = new JList(min);
+		JList<String> list = new JList<String>(min);
 		list.setVisibleRowCount(7);
 
 		int[] ind = new int[1];
@@ -171,27 +275,24 @@ public class ViewPanel {
 		
 		GridBagConstraints c = new GridBagConstraints();
 		
-		offset -=2;
 		
-		c.gridx = 2;
-		c.gridy = offset++;
+		c.gridx = 1;
+		c.gridy = 3;
 		c.gridwidth = 1;
-		c.anchor = GridBagConstraints.CENTER;
-		c.insets = new Insets(10, 0, 0, 0);
-		
+		c.insets = new Insets(10,5,0,5);
 
 		
 		panel.add(new JLabel("Minute"),c);
-		
+		c.insets = new Insets(0,5,10,5);
 		//c.anchor = GridBagConstraints.WEST;
 
-		c.gridy = offset++;
+		c.gridy = 4;
 		
 		panel.add(sp, c);
 	}
 	
 	
-	public void addHoursList()
+	public void addHoursList(JPanel panel)
 	{
 		String[] hrs = new String[range.hours.length+1];
 		hrs[0] = "Select all";
@@ -199,7 +300,7 @@ public class ViewPanel {
 		{
 			hrs[i] = ""+(i-1);
 		}
-		JList list = new JList(hrs);
+		JList<String> list = new JList<String>(hrs);
 		list.setVisibleRowCount(7);
 
 		int[] ind = new int[1];
@@ -210,33 +311,31 @@ public class ViewPanel {
 		
 		GridBagConstraints c = new GridBagConstraints();
 		
-		c.gridx = 1;
-		c.gridy = offset++;
+		c.gridx = 0;
+		c.gridy = 3;
 		c.gridwidth = 1;
-		c.anchor = GridBagConstraints.CENTER;
-		c.insets = new Insets(10, 0, 0, 0);
-		
+		c.insets = new Insets(10,5,0,5);
 
 		
 		panel.add(new JLabel("Hour"),c);
-		
+		c.insets = new Insets(0,5,10,5);
 		//c.anchor = GridBagConstraints.WEST;
 
-		c.gridy = offset++;
+		c.gridy = 4;
 		
 		panel.add(sp, c);
 	}
 	
 	
-	public void addDaysList()
+	public void addDaysList(JPanel panel)
 	{
 		String[] days = new String[range.daysOfWeek.length+1];
 		days[0] = "Select all";
 		for(int i = 1; i < days.length; i++)
 		{
-			days[i] = ""+DateRange.getDay(i-1);
+			days[i] = ""+DateRange.getDay(i-1).substring(0,3);
 		}
-		JList list = new JList(days);
+		JList<String> list = new JList<String>(days);
 		list.setVisibleRowCount(7);
 
 		int[] ind = new int[1];
@@ -248,27 +347,24 @@ public class ViewPanel {
 		
 		GridBagConstraints c = new GridBagConstraints();
 
-		offset -= 2;
 		
-		c.gridx = 3;
-		c.gridy = offset++;
+		c.gridx = 2;
+		c.gridy = 1;
 		c.gridwidth = 1;
-		c.anchor = GridBagConstraints.CENTER;
-		c.insets = new Insets(10, 0, 0, 0);
-		
+		c.insets = new Insets(10,5,0,5);
 
 		
 		panel.add(new JLabel("Day"),c);
-		
+		c.insets = new Insets(0,5,0,5);
 		//c.anchor = GridBagConstraints.WEST;
-		c.gridy = offset++;
-		c.anchor = GridBagConstraints.WEST;
+		c.gridy = 2;
+		c.anchor = GridBagConstraints.CENTER;
 
 		panel.add(sp, c);
 	}
 	
 	
-	public void addMonthsList()
+	public void addMonthsList(JPanel panel)
 	{
 		String[] mos = new String[range.months.length+1];
 		mos[0] = "Select all";
@@ -276,7 +372,7 @@ public class ViewPanel {
 		{
 			mos[i] = DateRange.getMonth(i-1).substring(0,3);
 		}
-		JList list = new JList(mos);
+		JList<String> list = new JList<String>(mos);
 		list.setVisibleRowCount(7);
 
 		int[] ind = new int[1];
@@ -284,30 +380,26 @@ public class ViewPanel {
 		list.setSelectedIndices(ind);
 		monthList = list;
 		JScrollPane sp = new JScrollPane(list);
-		sp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
 		GridBagConstraints c = new GridBagConstraints();
 
-		offset -= 2;
 		
-		c.gridx = 2;
-		c.gridy = offset++;
+		c.gridx = 1;
+		c.gridy = 1;
 		c.gridwidth = 1;
-		c.anchor = GridBagConstraints.CENTER;
-		c.insets = new Insets(10, 0, 0, 0);
-		
+		c.insets = new Insets(10,5,0,5);
 
 		
 		panel.add(new JLabel("Month"),c);
 		//c.anchor = GridBagConstraints.WEST;
-
-		c.gridy = offset++;
+		c.insets = new Insets(0,5,0,5);
+		c.gridy = 2;
 		
 		panel.add(sp, c);
 	}
 	
 	
-	public void addYearList()
+	public void addYearList(JPanel panel)
 	{
 		String[] yrs = new String[range.years.length+1];
 		int year = range.minYear;
@@ -317,7 +409,7 @@ public class ViewPanel {
 			yrs[i] = ""+year;
 			year++;
 		}
-		JList list = new JList(yrs);
+		JList<String> list = new JList<String>(yrs);
 		
 		list.setVisibleRowCount(7);
 		int[] ind = new int[1];
@@ -328,34 +420,16 @@ public class ViewPanel {
 		
 		GridBagConstraints c = new GridBagConstraints();
 
-		JLabel title = new JLabel("  Select files to include, by...");
-		
-		
+		c.gridy = 1;
 		c.gridx = 0;
-		c.gridy = offset++;
-		c.gridwidth = 4;
-		c.anchor = GridBagConstraints.WEST;
-		c.insets = new Insets(10, 0, 0, 0);
-		
-		panel.add(new JLabel(" "),c);
-		c.gridy = offset++;
-		panel.add(title, c);
-		
-		c.anchor = GridBagConstraints.CENTER;
-
-
-		
-		c.gridy = offset++;
-		c.gridx = 1;
 		c.gridwidth = 1;
+		c.insets = new Insets(10,5,0,5);
 		
 		panel.add(new JLabel("Year"),c);
 		
-		c.gridy = offset++;
-		c.fill = GridBagConstraints.VERTICAL;
-		//c.anchor = GridBagConstraints.WEST;
-
+		c.insets = new Insets(0,5,0,5);
 		
+		c.gridy = 2;
 		panel.add(sp, c);
 	}
 
@@ -369,163 +443,95 @@ public class ViewPanel {
 		return c;
 	}
 	
-	public void addTitle()
-	{
-		GridBagConstraints c = new GridBagConstraints();
-		
-		String toAppendToTitle = "";
-		
-		c.gridx = 0;
-		c.gridy = offset++;
-		c.gridwidth = 4;
-		
-		
-		if(title.indexOf("Single")==-1)
-		{
-			String[] choices = new String[2];
-			choices[0] = "Graph 1 (top)";
-			choices[1] = "Graph 2 (bottom)";
-			JComboBox box = new JComboBox(choices);
-			
-		if(title.indexOf("1") != -1)
-		{
-			box.setSelectedIndex(0);
-			box.addActionListener(new SwitchSidePanelAction(m));
-			
-			c.gridwidth = 3;
-			c.gridx = 1;
-			panel.add(box,c);
-			
-			c.gridy = offset++;
-			panel.add(new JLabel(" "),c);
-			
-			c.gridy = offset++;
-			c.gridx = 0;
-			c.gridwidth = 4;
-			toAppendToTitle="IN GRAPH 1 ";
-		}
-		else if(title.indexOf("2") != -1)
-		{
-			
-			box.setSelectedIndex(1);
-			box.addActionListener(new SwitchSidePanelAction(m));
-			c.gridwidth = 3;
-			c.gridx = 1;
-			panel.add(box,c);
-			
-			c.gridy = offset++;
-			panel.add(new JLabel(" "),c);
-			
-			c.gridy = offset++;
-			c.gridx = 0;
-			c.gridwidth = 4;
-			toAppendToTitle = "IN GRAPH 2 ";
-		}
-		}
-		
-		String label = "  View files "+toAppendToTitle+"by...";
-		
-		
-		JLabel title = new JLabel(label);
-		
-		
-		c.anchor = GridBagConstraints.WEST;
-
-		panel.add(title, c);
-		
-	}
-
-	public void addSeconds()
-	{
-		GridBagConstraints c = new GridBagConstraints();
 	
-		c.gridx = 3;
-		c.gridy = offset++;
-		c.gridwidth = 1;
-		c.anchor = GridBagConstraints.WEST;
 
-		c.insets = new Insets(10, 0, 0, 0);
-		secondsButton.setBackground(this.c);
-		secondsButton.addActionListener(new ViewSelectAction(secondsButton, range, DateRange.Grouping.SECONDS));
-		panel.add(secondsButton, c);
-	}	
-	
-	public void addMinutes()
+	public void addSeconds(JPanel panel)
 	{
 		GridBagConstraints c = new GridBagConstraints();
 	
 		c.gridx = 2;
-		c.gridy = offset;
-		c.gridwidth = 1;
+		c.gridy = 2;
+		c.weightx = 1;
+		c.weighty = 1;
 		c.anchor = GridBagConstraints.WEST;
-
-		c.insets = new Insets(10, 0, 0, 0);
+		c.insets = new Insets(0,10,10,10);
+		secondsButton.setBackground(this.c);
+		secondsButton.addActionListener(new ViewSelectAction(secondsButton, range, DateRange.Grouping.SECONDS));
+		panel.add(secondsButton, c);
+	
+		
+	}	
+	
+	public void addMinutes(JPanel panel)
+	{
+		GridBagConstraints c = new GridBagConstraints();
+	
+		c.gridx = 1;
+		c.gridy =2;
+		c.weightx = 1;
+		c.weighty = 1;
+		c.anchor = GridBagConstraints.WEST;
+		c.insets = new Insets(0,10,10,10);
 		minutesButton.setBackground(this.c);
 		minutesButton.addActionListener(new ViewSelectAction(minutesButton, range, DateRange.Grouping.MINUTES));
 		panel.add(minutesButton, c);
 	}	
 	
-	public void addHours()
+	public void addHours(JPanel panel)
 	{
 		GridBagConstraints c = new GridBagConstraints();
 	
-		c.gridx = 1;
-		c.gridy = offset;
-		c.gridwidth = 1;
+		c.gridx = 0;
+		c.gridy = 2;
+		c.weightx = 1;
+		c.weighty = 1;
 		c.anchor = GridBagConstraints.WEST;
-
-		c.insets = new Insets(10, 0, 0, 0);
+		c.insets = new Insets(0,10,10,10);
 		hoursButton.setBackground(this.c);
 		hoursButton.addActionListener(new ViewSelectAction(hoursButton, range, DateRange.Grouping.HOURS));
 		panel.add(hoursButton, c);
 	}	
 	
-	public void addDaysOfWeek()
+	public void addDaysOfWeek(JPanel panel)
 	{
 		GridBagConstraints c = new GridBagConstraints();
-		c.gridx = 3;
-		c.gridy = offset++;
-		c.gridwidth = 1;
+		c.gridx = 2;
+		c.gridy =1;
+		c.weightx = 1;
+		c.weighty = 1;
 		c.anchor = GridBagConstraints.WEST;
-
-		c.insets = new Insets(10, 0, 0, 0);
+		c.insets = new Insets(10,10,0,10);
 		daysOfWeekButton.setBackground(this.c);
 		daysOfWeekButton.addActionListener(new ViewSelectAction(daysOfWeekButton, range, DateRange.Grouping.DAYS_OF_WEEK));
 		panel.add(daysOfWeekButton, c);
-		
-
 	}
 
-	public void addMonths()
+	public void addMonths(JPanel panel)
 	{
 		GridBagConstraints c = new GridBagConstraints();
 		
-		c.gridx = 2;
-		c.gridy = offset;
-		c.gridwidth = 1;
+		c.gridx = 1;
+		c.gridy = 1;
+		c.weightx = 1;
+		c.weighty = 1;
 		c.anchor = GridBagConstraints.WEST;
-		c.insets = new Insets(10, 0, 0, 0);
+		c.insets = new Insets(10,10,0,10);
 		monthsButton.setBackground(this.c);
 		monthsButton.addActionListener(new ViewSelectAction(monthsButton, range, DateRange.Grouping.MONTHS));
 		panel.add(monthsButton, c);
 
 	}
 
-	public void addYears()
+	public void addYears(JPanel panel)
 	{
 		GridBagConstraints c = new GridBagConstraints();
 		
 		c.gridx = 0;
-		c.gridy = offset;
-		c.gridwidth = 1;
-		panel.add(new JLabel("  "), c);
-		
-		
-		c.gridx = 1;
-		c.gridy = offset;
-		c.gridwidth = 1;
+		c.gridy = 1;
+		c.weightx = 1;
+		c.weighty = 1;
 		c.anchor = GridBagConstraints.WEST;
-		c.insets = new Insets(10, 0, 0, 0);
+		c.insets = new Insets(10,10,0,10);
 		yearsButton.setSelected(true);
 		yearsButton.setBackground(this.c);
 		yearsButton.addActionListener(new ViewSelectAction(yearsButton, range, DateRange.Grouping.YEARS));
