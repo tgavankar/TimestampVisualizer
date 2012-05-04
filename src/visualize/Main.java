af9afdacb2e5d785973f582127d206639695196d
Lillian Michalek <lmichale@andrew.cmu.edu>
2012-05-04 18:59:59 -0400

    Cleaned up dead code in visualizer

--- src/visualize/Main.java
+++ src/visualize/Main.java
@@ -1,10 +1,27 @@
 package visualize;
 
+import org.jfree.ui.RefineryUtilities;
+
+import panels.MasterPanel;
+import actions.AddNewDataSetAction;
+
+
 
 public class Main 
-{	
-	 public static void main(String[] args) 
-	 {
-		 new Controller();
-	 }
+{ 
+  public static void main(String[] args) 
+  {
+    
+    
+    MasterPanel display;
+    
+      display = new MasterPanel();
+      display.pack();
+      RefineryUtilities.centerFrameOnScreen(display);
+      display.setResizable(false);
+      display.setVisible(true);
+      (new AddNewDataSetAction(display)).actionPerformed(null);
+      
+    
+  }
 }
